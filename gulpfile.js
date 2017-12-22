b12e869a60edee379f72145629f500e35827fa4a
michaele1 <mickel13@gmail.com>
2017-12-22 04:19:49 +0000

    completed lessons 9-10

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssvars = require('postcss-simple-vars');
+const nested = require('postcss-nested');
+
+gulp.task('default', () => {
+    console.log('Yay');
+});
+
+gulp.task('html', () => {
+    console.log('Imagine something useful for HTML here');
+});
+
+gulp.task('styles', () => {
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssvars, nested, autoprefixer]))
+        .pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', () => {
+    watch('./app/index.html', { usePolling: true }, () => {
+        gulp.start('html');
+    });
+
+    watch('./app/assets/styles/**/*.css', { usePolling: true }, () => {
+        gulp.start('styles');
+    });
+});
