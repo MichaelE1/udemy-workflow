af45d4be4eb6920e7cd65d70373d2e6adb76fef8
Michael <mickel13@gmail.com>
2018-01-06 16:18:27 +1000

    completed lesson 38

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,13 +1,10 @@
-function Person(fullName, favColour) {
-    this.name = fullName;
-    this.favouriteColour = favColour;
-    this.greet = function() {
-        console.log("Hello, my name is " + this.name + " and my favourite colour is " + this.favouriteColour);
-    }
-}
+var $ = require('jquery');
+var Person = require('./modules/Person');
 
 var john = new Person("John Doe", "blue");
 john.greet();
 
 var jane = new Person("Jane Smith", "green");
 jane.greet();
+
+$("h1").remove();
